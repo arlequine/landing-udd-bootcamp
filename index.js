bf2064e58bc02d265f8846ebe2319b25cca0a0e1
Alfredo Hernandez Noriega <alfredo.hernandez@kushkipagos.com>
2022-01-22 09:44:08 -0600

    commit inicial

---
+++ index.js
@@ -0,0 +1,92 @@
+let variable = 'soy una variable'
+
+const constante = 3.1416;
+
+let bool = true
+
+let persona = {
+  nombre: 'fredo',
+  edad: 15,
+  mayorDeEdad: true,
+}
+
+// console.log(persona.nombre);
+
+// if (persona.edad >= 18) {
+//   console.log('Es mayor de edad')
+// } else {
+//   console.log('No es mayor de edad')
+// }
+
+let lista = ['fresa', 'sandia', 'naranja', 'melon'];
+
+// for (let index = 0; index < lista.length; index++) {
+//   console.log(lista[index])
+// }
+
+function saludo(nombre) {
+  console.log('Hola: ' + nombre);
+}
+
+// saludo('Cristobal');
+
+// console.log('Hello World')
+
+
+/*
+  Soy un comentario
+*/
+
+
+
+// let elemento = document.querySelector('.container')
+
+// console.log(elemento);
+
+
+// Funciones de carrusel
+
+const delay = 3000
+
+const slides = document.querySelector('.slides');
+console.log('slides', slides);
+
+const slidesCount = slides.childElementCount
+
+console.log('slidesCount', slidesCount);
+
+const maxLeft = (slidesCount - 1) * 100 * -1
+
+let current = 0;
+
+function changeSlide(next = true) {
+  if (next) {
+    current += current > maxLeft ? -100 : current * -1;
+  } else {
+    current = current < 0 ? current + 100 : maxLeft;
+  }
+  // bacticks
+  // slides.style.left = `${current}%` 
+  slides.style.left = current + '%'
+}
+
+let autoChange = setInterval(changeSlide, delay)
+
+function restart() {
+  clearInterval(autoChange);
+  autoChange = setInterval(changeSlide, delay)
+}
+
+// controls
+
+document.querySelector(".next-slide").addEventListener("click", function () {
+  changeSlide();
+  restart();
+})
+document.querySelector(".prev-slide").addEventListener("click", function () {
+  changeSlide(false);
+  restart();
+})
+
+
+
